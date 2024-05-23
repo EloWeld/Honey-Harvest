738f793ade545ee80d19927124952bc0a330985d
EloWeld <51724400+EloWeld@users.noreply.github.com>
2024-05-23 11:08:46 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-900">
+          Hello, Tailwind CSS with Next.js!
+        </h1>
+      </div>
+    )
+  }
+  
